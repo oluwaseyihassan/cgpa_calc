e5296de9ebe5b2345f9458fb362a19baaa2b80c5
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2025-12-25 15:57:48 +0100

    added pwa register

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 import App from './App.vue'
 import router from './router'
