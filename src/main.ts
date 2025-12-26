988b7cc50c3adb9fafadea5f955bb3e5221c8798
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2025-12-26 16:27:58 +0100

    installed and initialized vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { registerSW } from 'virtual:pwa-register'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 registerSW({ immediate: true })
 
